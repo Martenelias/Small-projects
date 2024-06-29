5c27d8fe991b694333d78c32140755dc40f72fa2
Marten Elias <marten.elias1998@gmail.com>
2024-06-30 01:59:01 +0300

    lets play animation

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+let letsPlay = document.getElementById("letsPlay");
 
+window.onload = function() {
+  document.getElementById("gameBtn").addEventListener("click", closeWindow);
+  document.getElementById("nextRound").disabled = true;
+  document.getElementById("playAgain").disabled = true;
+}
+
+function closeWindow() {
+  letsPlay.classList.add("slide-out");
+  setTimeout(() => {
+    letsPlay.style.display = "none"
+  }, 1000);
+}
