863e22c8eaee1a7e5e518e4a338bf88f90c1f1d2
Marten Elias <marten.elias1998@gmail.com>
2024-07-01 17:32:33 +0300

    rock paper and scissors buttons work

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -2,6 +2,9 @@ let letsPlay = document.getElementById("letsPlay");
 let usernameInput = document.getElementById("username");
 let errorText = document.getElementById("errorText");
 let gameBtn = document.getElementById("gameBtn");
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
 
 let username = "";
 
@@ -10,7 +13,10 @@ window.onload = function() {
   usernameInput.addEventListener("input", getUsername)
   document.getElementById("nextRound").style.display = "none";
   document.getElementById("playAgain").style.display = "none";
-}
+  rock.addEventListener("click", () => getPlayerChoice("rock"));
+  paper.addEventListener("click", () => getPlayerChoice("paper"));
+  scissors.addEventListener("click", () => getPlayerChoice("scissors"));
+};
 
 function getUsername() {
   username = usernameInput.value;
@@ -26,7 +32,7 @@ function getUsername() {
     errorText.textContent = "";
     gameBtn.disabled = false;
   }
-}
+};
 
 
 function closeWindow() {
@@ -34,8 +40,39 @@ function closeWindow() {
     letsPlay.classList.add("slide-out");
     setTimeout(() => {
       letsPlay.style.display = "none"
+      document.getElementById("nextRound").style.display = "block";
+      document.getElementById("myName").textContent = username;
     }, 1000);
-    document.getElementById("nextRound").style.display = "block";
-    let myName = document.getElementById("myName").textContent = username;
+  }
+};
+
+function getPlayerChoice(playerChoice) {
+  const computerChoice = getComputerChoice();
+  const result = getWinner(playerChoice, computerChoice);
+  displayResult(result);
+};
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+};
+
+function getWinner(playerOne, computer) {
+  if(playerOne === computer) {
+    return "Tie Game!";
+  } else if (
+    (playerOne === "rock" && computer === "scissors") ||
+    (playerOne === "paper" && computer === "rock") ||
+    (playerOne === "scissors" && computer === "paper") 
+  ) {
+    return `${username} wins!`;
+  } else {
+    return "computer wins!";
   }
 }
+
+
+function displayResult(result) {
+  document.getElementById("resultText").textContent = result;
+};
