1b2b34fda83f73adfc6b27fdc37e24a00f782f53
Marten Elias <marten.elias1998@gmail.com>
2024-07-03 13:01:59 +0300

    starting with app

--- TetrisGame/app.js
+++ TetrisGame/app.js
@@ -1,2 +1,21 @@
-// add 200 divs.
-// install ESlint
+const iTetromino = "iShape";
+const jTetromino = "jShape";
+const lTetromino = "lShape";
+const oTetromino = "oShape";
+const zTetromino = "zShape";
+const tTetromino = "tShape";
+const sTetromino = "sShape";
+const tetrominoes = [iTetromino, jTetromino, lTetromino, oTetromino, zTetromino, tTetromino, sTetromino];
+const scoreDisplay = document.getElementById("score");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("tetris-container");
+
+  for (let i = 0; i < 200; i++) {
+    const div = document.createElement("div");
+    div.classList.add("tetris-box");
+    container.appendChild(div);
+  }
+  let squares = Array.from(document.querySelectorAll(".tetris-box"));
+  console.log(squares);
+});
