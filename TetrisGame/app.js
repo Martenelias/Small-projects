3edf3af3be31168f43b286a2696a04921ca94dfe
Marten Elias <marten.elias1998@gmail.com>
2024-07-04 17:14:45 +0300

    displayShapes and remove bottom grid

--- TetrisGame/app.js
+++ TetrisGame/app.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("tetris-container");
+const nextShape = document.getElementById("nextShape");
 
 for (let i = 0; i < 200; i++) {
   const div = document.createElement("div");
@@ -11,21 +12,30 @@ for (let i = 0; i < 10; i++) {
   container.appendChild(div);
 };
 
+for (let i = 0; i < 16; i++) {
+  const div = document.createElement("div");
+  div.classList.add("next-shape");
+  nextShape.appendChild(div);
+};
+
 const scoreDisplay = document.getElementById("score");
 const startBtn = document.getElementById("startBtn");
 let squares = Array.from(document.querySelectorAll(".tetris-box"));
+let displaySquares = Array.from(document.querySelectorAll(".next-shape"));
+const displayWidth = 4;
+let displayIndex = 0;
 const width = 10;
-let nexRound = 0;
+let nextRandom = 0;
 let timerId;
 let score = 0;
 const colors = [
-  '#ecb5ff',
-  '#ffa0ab',
-  '#8cffb4',
-  '#ff8666',
-  '#80c3f5',
-  '#c2e77d',
-  '#fdf9a1',
+  "#ecb5ff",
+  "#ffa0ab",
+  "#8cffb4",
+  "#ff8666",
+  "#80c3f5",
+  "#c2e77d",
+  "#fdf9a1",
 ];
 
 const lTetromino = [
@@ -61,6 +71,14 @@ const iTetromino = [
   [width,width+1,width+2,width+3],
   [1,width+1,width*2+1,width*3+1],
   [width,width+1,width+2,width+3]
+];
+
+const nextUpTetrominos = [
+  [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+  [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+  [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+  [0, 1, displayWidth, displayWidth+1], //oTetromino
+  [1, displayWidth+1, displayWidth*2+1,  displayWidth*3+1] //iTetromino
 ];
 
 const tetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
@@ -105,15 +123,16 @@ const moveDown = () => {
   freeze();
 };
 
-timerId = setInterval(moveDown, 1000);
-
 const freeze = () => {
   if (current.some(index => squares[currentPosition + index + width].classList.contains("bottom"))) {
     current.forEach(index => squares[currentPosition + index].classList.add("bottom"));
-    random = Math.floor(Math.random() * tetrominoes.length);
+    random = nextRandom;
+    nextRandom = Math.floor(Math.random() * tetrominoes.length);
     current = tetrominoes[random][currentRotation];
     currentPosition = 4;
     draw();
+    displayShape();
+    addScore();
   }
 };
 
@@ -135,4 +154,76 @@ const moveRight = () => {
     currentPosition -= 1;
   }
   draw();
-}
+};
+
+
+const isAtRight = () => {
+  return current.some(index => (currentPosition + index + 1) % width === 0);
+};
+
+const isAtLeft = () => {
+  return current.some(index => (currentPosition + index) % width === 0);
+};
+
+const checkRotatePosition = (P) => {
+  P = P || currentPosition;
+  if ((P + 1) % width < 4) {
+    if (isAtRight()) {
+      currentPosition += 1;
+      checkRotatePosition(P);
+    }
+  }
+  else if (P % width > 5) {
+    if (isAtLeft()) {
+      currentPosition += 1;
+      checkRotatePosition(P);
+    }
+  }
+};
+
+const rotate = () => {
+  undraw();
+  currentRotation ++;
+  if (currentRotation === current.length) {
+    currentRotation = 0;
+  }
+  current = tetrominoes[random][currentRotation];
+  checkRotatePosition();
+  draw();
+};
+
+const displayShape = () => {
+  displaySquares.forEach(squares => {
+    squares.classList.remove("tetromino")
+  });
+  nextUpTetrominos[nextRandom].forEach(index => {
+    displaySquares[displayIndex + index].classList.add("tetromino")
+  });
+};
+
+startBtn.addEventListener("click", () => {
+  if (timerId) {
+    clearInterval(timerId);
+    timerId = null;
+  } else {
+    draw();
+    timerId = setInterval(moveDown, 1000);
+    displayShape();
+  }
+});
+
+const addScore = () => {
+  for (let i = 0; i < 199; i += width) {
+    const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
+    if (row.every(index => squares[index].classList.contains("bottom"))) {
+      score += 10;
+      scoreDisplay.innerHTML = score;
+      row.forEach(index => {
+        squares[index].classList.remove("bottom")
+      });
+      const squaresRemoved = squares.splice(i, width);
+      squares = squaresRemoved.concat(squares);
+      squares.forEach(cell => container.appendChild(cell));
+    }
+  }
+};
